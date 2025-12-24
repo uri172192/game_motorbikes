6806838092958d092fce43ef5176c21301a04f95
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2025-12-24 19:03:10 +0100

    Update joc_motos.py

--- joc_motos.py
+++ joc_motos.py
@@ -4,12 +4,11 @@ from datetime import datetime, timedelta
 import unicodedata
 import os
 
-BASE_DIR = os.getcwd()  # directori base per Streamlit
+BASE_DIR = os.getcwd()  # Directori base per Streamlit
 
 # -------------------------
 # Funcions
 # -------------------------
-
 def dia_del_joc():
     ara = datetime.now()
     if ara.hour < 8:
@@ -24,36 +23,37 @@ def normalitza(text):
 # -------------------------
 # Carregar dades
 # -------------------------
-
-pilots = pd.read_csv("pilots.csv")
+pilots = pd.read_csv("pilots.csv", sep=";")
 st.write("Columnes del CSV:", pilots.columns)
 st.write("Primeres files:", pilots.head())
+
 index = dia_del_joc().toordinal() % len(pilots)
 pilot_dia = pilots.iloc[index]
 
 # -------------------------
 # Estat del joc
 # -------------------------
-
 if "encertat" not in st.session_state:
     st.session_state.encertat = False
 
 # -------------------------
 # UI
 # -------------------------
-
-# Comprova que hi ha pilots carregats
 if len(pilots) == 0:
     st.error("Error: pilots.csv està buit o no s'ha trobat.")
 else:
-    index = dia_del_joc().toordinal() % len(pilots)
-    pilot_dia = pilots.iloc[index]
-
     st.title("🏍️ Pilot del dia")
     st.image(
         os.path.join(BASE_DIR, "Fotos", pilot_dia["image"]),
         use_container_width=True
     )
 
-
-
+    guess = st.text_input("Quin pilot és?")
+    if guess:
+        if normalitza(guess) == normalitza(pilot_dia["name"]):
+            st.session_state.encertat = True
+            st.success("✅ Correcte! Has encertat el pilot del dia!")
+            st.balloons()
+            st.stop()
+        else:
+            st.error("❌ No és correcte, torna-ho a provar!")
